84f5f49d27ecffba9a0c7b55c08a209e05cfd8a6
kmja <karl.mj.andersson@gmail.com>
2017-02-09 12:22:04 +0100

    more furniture + edge detection on disks

---
+++ ToVR of Hanoi/Assets/Scripts/EdgeTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeTrigger : MonoBehaviour {
+	// Attached to box collider surrounding play area. If a disk leaves the area, bounce it back
+
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("nu lämnade nåt");
+		// If object exiting is not rubble, shoot it back
+		if(other.gameObject.tag != "rubble") {
+			// Placeholder: Drop the disk randomly on the table. Later, aim for the player's head
+			Vector3 target = GameObject.Find("Middle").transform.position;
+			float randRangeMin = -5f;
+			float randRangeMax = 5f;
+			other.gameObject.GetComponent<Rigidbody> ().MovePosition (new Vector3(Random.Range(randRangeMin, randRangeMax), 10f, Random.Range(randRangeMin, randRangeMax)));
+		}
+	}
+}
