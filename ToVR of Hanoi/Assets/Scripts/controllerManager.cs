6b8ad758d1c727bcf65ed8ee6060f786da5fd6de
kmja <karl.mj.andersson@gmail.com>
2017-02-20 12:18:23 +0100

    script for controller input

---
+++ ToVR of Hanoi/Assets/Scripts/controllerManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllerManager : MonoBehaviour {
+	
+	void Update() {
+		// Log something when each trigger is pressed 
+		if(Input.GetButtonDown("14")) {
+			Debug.Log ("Left trigger down");
+
+		}
+		if(Input.GetButtonDown("15")) {
+			Debug.Log ("Right trigger down");
+
+		}
+		// Grab object
+
+	}
+
+	void OnTriggerStay(Collider other) {
+		// Lock position of disk to controller while their colliders are touching and the trigger is pressed
+		if (other.transform.parent.name == "Disks" && Input.GetButton("15")) {
+			other.gameObject.transform.position = transform.position;
+		}
+	}
+
+}
+
